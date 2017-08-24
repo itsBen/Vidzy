c8c676a7b7dcf364776b81a988a2edb7f89aa733
itsBen <benedikt.zneb@gmail.com>
2017-08-24 09:37:06 +0300

    implemented feature to add a video

--- public/javascripts/vidzy.js
+++ public/javascripts/vidzy.js
@@ -6,6 +6,10 @@ app.config(['$routeProvider', function($routeProvider){
             templateUrl: 'partials/home.html',
             controller: 'HomeCtrl'
         })
+        .when('/add-video', {
+            templateUrl: 'partials/video-form.html',
+            controller: 'AddVideoCtrl'
+        })        
         .otherwise({
             redirectTo: '/'
         });
@@ -16,4 +20,14 @@ app.controller('HomeCtrl', ['$scope', '$resource', function($scope, $resource){
     Videos.query(function(videos){
         $scope.videos = videos;
     });
+}]);
+
+app.controller('AddVideoCtrl', ['$scope', '$resource', '$location',
+function($scope, $resource, $location){
+    $scope.save = function(){
+        var Videos = $resource('/api/videos');
+        Videos.save($scope.video, function(){
+            $location.path('/');
+        });
+    };
 }]);
